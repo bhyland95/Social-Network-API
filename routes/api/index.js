9dfb5fc9550203d3a0fa282395d43975348caae3
BH <blakehyland95@gmail.com>
2021-11-21 17:19:40 -0700

    small fix to api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-route');
+const thoughtRoutes = require('./thought-route');
 
 // add prefix of `/users` to routes created in `user-routes.js`
 router.use('/users', userRoutes);
