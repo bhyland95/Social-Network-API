937b381a984241da162eb2a38de04cffdfd5c43a
BH <blakehyland95@gmail.com>
2021-11-28 13:59:42 -0600

    pseudocode for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,20 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+    //get all thoughts 
+
+    //get single thought
+
+    //new thought 
+
+    //update thought
+
+    //delete thought
+
+    //add reaction 
+
+    //remove reaction
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+//get all users
+
+//get single user 
+
+//new user 
+
+//update user 
+
+//delete user
+
+//add friend 
+
+//remove friend 
+};
+
+module.exports = userController;
